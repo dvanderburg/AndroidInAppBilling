a42c2b385264e5f708eb9ceb500caf04a59391ae
Dave Vanderburg <dave@hitgrab.com>
2013-06-10 16:16:26 -0300

    Added missing import for org.json.JSONObject

--- v3/com/smartmobilesoftware/inappbilling/InAppBillingPlugin.java
+++ v3/com/smartmobilesoftware/inappbilling/InAppBillingPlugin.java
@@ -7,6 +7,7 @@ import org.apache.cordova.api.CallbackContext;
 import org.apache.cordova.api.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.smartmobilesoftware.util.Purchase;
 import com.smartmobilesoftware.util.IabHelper;
